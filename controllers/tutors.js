a5d15084ba4d9d3b9d568c276e9df33d365d6b70
Parth Bhore <bhoreparth@gmail.com>
2022-03-10 01:15:11 +0530

    Added create/find courses by tutors

--- controllers/tutors.js
+++ controllers/tutors.js
@@ -1,16 +1,34 @@
 const Course = require("../models/courses");
 
-const getAllCourses = (req, res) => {
-  res.send("Get all courses");
+//This contains functions of all routes accessed by the tutor, which
+//includes getting all courses by that tutor, creating/updating/deleting a course of that logged in tutor.
+
+const getAllCourses = async (req, res) => {
+  try {
+    const all_courses = await Course.find({});
+    res.status(200).json(all_courses); // this returns an array. Use {all_courses} to return class/object
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
 
-const getCourse = (req, res) => {
-  res.send("Get a course");
+const getCourse = async (req, res) => {
+  try {
+    const { id: courseID } = req.params;
+    const course = await Course.findOne({ _id: courseID });
+    res.status(200).json(course); // this returns an array. Use {all_courses} to return class/object
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
 
 const createCourse = async (req, res) => {
-  const course = await Course.create(req.body);
-  res.status(201).json(course);
+  try {
+    const course = await Course.create(req.body);
+    res.status(201).json(course);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
 
 const updateCourse = (req, res) => {
--- models/courses.js
+++ models/courses.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
 const CourseSchema = new mongoose.Schema({
-  name: String,
-  description: String,
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model("Course", CourseSchema);
